29058c3bb2b4493f4437b7c29039d50cf66f4832
Briggoh <H.e.briggs99@gmail.com>
2022-11-21 16:36:15 -0800

    added near finishing touches on html. started javascript

---
+++ app.js
@@ -0,0 +1,9 @@
+// Declaring a variable that will store the user-inputted city
+let city ="";
+
+// Variable declaration
+
+
+
+//setting up my API key 
+let APIKey = "f5463c710a507bc638be3354a58139dd"
